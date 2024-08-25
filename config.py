104b6cf90d7389f81054382e4a87168856137316
Araydevelop <161925462+Araydevelop@users.noreply.github.com>
2024-08-25 23:19:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LiNK_X_NeTWoRK_Here")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/LiNK_X_NeTWoRK_Here")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Shivang_xd")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Shivang_xd")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
